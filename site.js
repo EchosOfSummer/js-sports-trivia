43468137500748784709531440d50961e1382036
SummerEcho <164468785+EchosOfSummer@users.noreply.github.com>
2025-10-27 18:09:28 -0500

    Completed exam. went down a rabbit hole with playing with the css and getting it be to different.

--- site.js
+++ site.js
@@ -12,10 +12,51 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 
 	// todo: create your "getNextQuestion" function
 
-	// todo: create your "renderQuestion" function
+	const getNextQuestion = async () => {
+		try {
+			const url = 'https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple'
+			const response = await fetch(url)
+			const json = await response.json()
+			const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+			const answers = shuffle([ ...incorrect, correct ])
+			return { question, answers, correct }
 
+		}catch (err) {
+			console.error('Error fetching question: ', err)
+			alert('Nope, something went wrong')
+		}
+	}
+	// console.log(await getNextQuestion())
+	// todo: create your "renderQuestion" function
+	const renderQuestion = ({ question, answers, correct }) => {
+		answersElement.innerHTML = ''
+		questionElement.innerHTML = decodeHtml(question)
+		questionElement.classList.add('glitch')
+		answers.forEach(answer => {
+			const btn = document.createElement('button')
+			btn.innerHTML = decodeHtml(answer)
+			btn.addEventListener('click', () => {
+				if (answer === correct) {
+					btn.classList.add('correct')
+					answersElement.querySelectorAll('button').forEach(b => b.disabled = true)
+					alert('Correct!')
+					return
+				}
+				btn.disabled = true
+				btn.classList.add('incorrect')
+				alert('Incorrect!')
+			})
+			answersElement.appendChild(btn)
+		})
+	}
+	// console.log(await renderQuestion(await getNextQuestion()))
 	// todo: add the event listener to the "nextQuestion" button
 
+	nextQuestionElement.addEventListener('click', async () => {
+		renderQuestion(await getNextQuestion())
+		nextQuestionElement.disabled = true
+		setTimeout(() => nextQuestionElement.disabled = false, 10000)
+	})
 })()
 
 // mimic a click on the "nextQuestion" button to show the first question
